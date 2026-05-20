104981ef84d2f79a7d9f4a04d9f2ec916868eb18
antoniosanp <antonio.pulgarin97@gmail.com>
2026-05-20 17:39:58 -0500

    first commit

---
+++ libreriaChat/src/main/java/com/utibunna/libreriaChat/LibreriaChatApplication.java
@@ -0,0 +1,13 @@
+package com.utibunna.libreriaChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaChatApplication.class, args);
+	}
+
+}
---
+++ libreriaChat/src/test/java/com/utibunna/libreriaChat/LibreriaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utibunna.libreriaChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
